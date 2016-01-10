ea3d510b641e5b38f216cba90d6aefa17f1f72b9
4jchc <jiangjin5y24@163.com>
2016-01-10 15:59:57 +0800

    1.0初始化

---
+++ 4jchc-Extension/4jchc-Extension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4jchc-Extension
+//
+//  Created by 蒋进 on 16/1/10.
+//  Copyright © 2016年 sijichcai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
